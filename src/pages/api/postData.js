5caa0a8434873d2802acb2ef7eac38dd45fa33fe
nukrobzero <nukrobzero@hotmail.com>
2023-03-30 16:19:46 +0700

    first commit

---
+++ src/pages/api/postData.js
@@ -0,0 +1,51 @@
+import { google } from "googleapis";
+
+export default async function handler(req, res) {
+  if (req.method !== "POST") {
+    return res.status(405).send({ message: "Only Post requests!" });
+  }
+
+  try {
+    const auth = new google.auth.GoogleAuth({
+      credentials: {
+        client_email: process.env.GOOGLE_CLIENT_EMAIL,
+        private_key: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+      },
+      scopes: [
+        "https://www.googleapis.com/auth/drive",
+        "https://www.googleapis.com/auth/drive.file",
+        "https://www.googleapis.com/auth/spreadsheets",
+      ],
+    });
+
+    const sheets = google.sheets({
+      auth,
+      version: "v4",
+    });
+
+    const { data } = req.body; // value from submit
+    const now = new Date();
+    const options = {
+      day: "2-digit",
+      month: "2-digit",
+      year: "2-digit",
+      hour: "2-digit",
+      minute: "2-digit",
+      second: "2-digit",
+    };
+    const formattedDateTime = now.toLocaleString("en-GB", options);
+
+    const response = await sheets.spreadsheets.values.append({
+      spreadsheetId: process.env.SPREADSHEET_ID,
+      range: "A1:D1",
+      valueInputOption: "USER_ENTERED",
+      requestBody: {
+        values: [[formattedDateTime, data]],
+      },
+    });
+    res.status(200).send(response);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Something went wrong");
+  }
+}
